07be2ee0b1ecd56d5f274f595bd8137914d6e249
Christian V. Danos <cris.danos@Christians-MacBook-Pro.local>
2024-08-07 21:41:44 +0800

    display of client info for the user_data_view

--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -24,67 +24,74 @@ export default async function Page() {
 
   const supabase = createClient();
 
-  const { data: usersList } = await supabase
+  const { data: usersList, error } = await supabase
     .from("users_data_view")
-    .select("user_id, name, email");
-  console.log(usersList, "userList here");
+    .select("user_id, client_name, email, privileges");
+
+  if (error) {
+    console.log(`Error Fetching Data: `, error);
+  }
+
+  console.log(usersList, "user list here");
 
   return (
-    <div className="py-16">
-      <Card>
-        <div className="flex justify-between items-center">
-          <TextInput
-            placeholder="Search by name"
-            color="primary"
-            className="w-[450px]"
-          />
-          <Button color="primary">Show All Users</Button>
-        </div>
-        <div className="mt-10 overflow-x-auto">
-          <Table hoverable>
-            <TableHead>
-              <TableHeadCell>Name</TableHeadCell>
-              <TableHeadCell>Email</TableHeadCell>
-              <TableHeadCell>Privileges</TableHeadCell>
-              <TableHeadCell className="w-32">
-                <span className="sr-only">View</span>
-              </TableHeadCell>
-            </TableHead>
-            <TableBody className="divide-y">
-              {usersList?.map(
-                (item: {
-                  user_id: string;
-                  name: string;
-                  email: string;
-                  privilege: Array<string>;
-                }) => (
-                  <TableRow key={item?.user_id} className="bg-white">
-                    <TableCell>{item?.name}</TableCell>
-                    <TableCell>{item?.email}</TableCell>
-                    <TableCell>
-                      <div className="flex gap-2">
-                        {item?.privilege?.map((p: string, i: number) => (
-                          <Badge key={i} className="w-fit" color="primary">
-                            {p}
-                          </Badge>
-                        ))}
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <Link
-                        href={`/user/${item.user_id}`}
-                        className="text-yellow-500 underline cursor-pointer"
-                      >
-                        View
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                )
-              )}
-            </TableBody>
-          </Table>
-        </div>
-      </Card>
-    </div>
+    !error && (
+      <div className="py-16">
+        <Card>
+          <div className="flex justify-between items-center">
+            <TextInput
+              placeholder="Search by name"
+              color="primary"
+              className="w-[450px]"
+            />
+            <Button color="primary">Show All Users</Button>
+          </div>
+          <div className="mt-10 overflow-x-auto">
+            <Table hoverable>
+              <TableHead>
+                <TableHeadCell>Name</TableHeadCell>
+                <TableHeadCell>Email</TableHeadCell>
+                <TableHeadCell>Privileges</TableHeadCell>
+                <TableHeadCell className="w-32">
+                  <span className="sr-only">View</span>
+                </TableHeadCell>
+              </TableHead>
+              <TableBody className="divide-y">
+                {usersList?.map(
+                  (item: {
+                    user_id: string;
+                    client_name: string;
+                    email: string;
+                    privileges: Array<string>;
+                  }) => (
+                    <TableRow key={item?.user_id} className="bg-white">
+                      <TableCell>{item?.client_name}</TableCell>
+                      <TableCell>{item?.email}</TableCell>
+                      <TableCell>
+                        <div className="flex gap-2">
+                          {item?.privileges?.map((p: string, i: number) => (
+                            <Badge key={i} className="w-fit" color="primary">
+                              {p}
+                            </Badge>
+                          ))}
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <Link
+                          href={`/user/${item.user_id}`}
+                          className="text-yellow-500 underline cursor-pointer"
+                        >
+                          View
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+            </Table>
+          </div>
+        </Card>
+      </div>
+    )
   );
 }
