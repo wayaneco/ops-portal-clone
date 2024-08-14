b147405fdc0beaadff23481f82e70b746777e7c3
Wayan Danyael Eco <wayan.eco@98labs.com>
2024-08-14 11:12:02 +0800

    revoke role integration

--- app/actions/user/revoke-privilege.ts
+++ app/actions/user/revoke-privilege.ts
@@ -1,8 +1,27 @@
 "use server";
 
-export async function revokePrivilege(data: {
+import { createClient } from "@/utils/supabase/server";
+import { revalidateTag, revalidatePath } from "next/cache";
+
+export async function revokePrivilege(params: {
   user_id: string;
   client_id: string;
+  staff_id: string;
 }) {
-  console.log(data, "=========");
+  const supabase = createClient();
+
+  const { data, error } = await supabase.rpc("revoke_all_user_roles", {
+    p_user_id: params.user_id,
+    p_client_id: params.client_id,
+    staff_id: params.staff_id,
+  });
+
+  revalidateTag("user_details");
+  revalidatePath("(dashboard)/user/[id]", "layout");
+
+  if (error) {
+    throw new Error("Error revoking roles.");
+  }
+
+  return data;
 }
