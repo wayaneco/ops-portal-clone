dd811d7fb6a462abe4949876b43035e14134a422
Christian V. Danos <cris.danos@Christians-MacBook-Pro.local>
2024-08-19 10:22:54 +0800

    removed wrong required field validation

--- app/(dashboard)/user/components/schema.ts
+++ app/(dashboard)/user/components/schema.ts
@@ -1,26 +1,21 @@
 import * as Yup from "yup";
 
 export const schema = Yup.object().shape({
-  photo_url: Yup.lazy((value) => {
-    if (value === null) {
-      return Yup.string().required("This field is required.");
-    }
-
-    return Yup.mixed().required("This field is required.").nullable();
+  photo_url: Yup.lazy((value: null) => {
+    return Yup.mixed().nullable();
   }),
-  first_name: Yup.string().required("This field is required."),
-  last_name: Yup.string().required("This field is required."),
-  middle_name: Yup.string().required("This field is required."),
+  first_name: Yup.string(),
+  last_name: Yup.string(),
+  middle_name: Yup.string(),
   birth_date: Yup.string(),
   preferred_name: Yup.string().required("This field is required."),
   email: Yup.string()
-    .required("This field is required.")
     .email("Must be a valid email address"),
-  primary_phone: Yup.string().required("This field is required."),
-  addr_line_1: Yup.string().required("This field is required."),
-  addr_line_2: Yup.string().required("This field is required."),
-  city: Yup.string().required("This field is required."),
-  zip_code: Yup.string().required("This field is required."),
-  country: Yup.string().required("This field is required."),
-  state_province_region: Yup.string().required("This field is required."),
+  primary_phone: Yup.string(),
+  addr_line_1: Yup.string(),
+  addr_line_2: Yup.string(),
+  city: Yup.string(),
+  zip_code: Yup.string().required('This field is required'),
+  country: Yup.string(),
+  state_province_region: Yup.string(),
 });
