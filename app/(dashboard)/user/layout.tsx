95a3429bcbfd995f418fc591b6c4162156749ae5
jeremik <>
2024-08-28 14:17:42 +0800

    update: hasPrivilege condition in user layout

--- app/(dashboard)/user/layout.tsx
+++ app/(dashboard)/user/layout.tsx
@@ -3,7 +3,7 @@
 import { PropsWithChildren } from "react";
 
 import { useUserClientProviderContext } from "@/app/components/Context/UserClientContext";
-import { ROLE_COMPANY_ADMIN, ROLE_NETWORK_ADMIN } from "@/app/constant";
+import { ROLE_COMPANY_ADMIN, ROLE_NETWORK_ADMIN, ROLE_AGENT } from "@/app/constant";
 import { redirect, usePathname } from "next/navigation";
 
 export default function Layout(props: PropsWithChildren) {
@@ -11,10 +11,12 @@ export default function Layout(props: PropsWithChildren) {
   const { currentPrivilege } = useUserClientProviderContext();
 
   const hasPrivilege = currentPrivilege?.some((privilege) =>
-    [ROLE_NETWORK_ADMIN, ROLE_COMPANY_ADMIN]?.includes(privilege)
+    [ROLE_NETWORK_ADMIN, ROLE_COMPANY_ADMIN, ROLE_AGENT]?.includes(privilege)
   );
 
-  if (!hasPrivilege && pathname === "/user") return redirect("/");
+  if (!hasPrivilege && pathname.includes("/user")) {
+    return redirect("/");
+  }
 
   return <>{props.children}</>;
 }
