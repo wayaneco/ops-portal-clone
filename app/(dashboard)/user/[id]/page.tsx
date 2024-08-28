240da11c7f1eb384b22acbf6cbff802e8ee5122e
jeremik <>
2024-08-28 14:47:29 +0800

    revert: code

--- app/(dashboard)/user/[id]/page.tsx
+++ app/(dashboard)/user/[id]/page.tsx
@@ -4,6 +4,7 @@ import { headers } from "next/headers";
 import { UserDetailForm } from "./components/user-detail-form";
 
 const Page = async function (props: { params: { id: string } }) {
+  console.log('🚀 ~ props:', props);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/user/${props?.params?.id}`,
     {
@@ -16,11 +17,13 @@ const Page = async function (props: { params: { id: string } }) {
     }
   );
 
+  console.log('🚀 ~ response:', response);
   if (!response.ok) {
     throw new Error(`Failed to fetch user ${props?.params.id}`);
   }
 
   const data = await response.json();
+  console.log('🚀 ~ data:', data);
 
   return (
     <div className="py-8 bg-gray-200">
--- app/(dashboard)/user/layout.tsx
+++ app/(dashboard)/user/layout.tsx
@@ -3,7 +3,7 @@
 import { PropsWithChildren } from "react";
 
 import { useUserClientProviderContext } from "@/app/components/Context/UserClientContext";
-import { ROLE_COMPANY_ADMIN, ROLE_NETWORK_ADMIN, ROLE_AGENT } from "@/app/constant";
+import { ROLE_COMPANY_ADMIN, ROLE_NETWORK_ADMIN } from "@/app/constant";
 import { redirect, usePathname } from "next/navigation";
 
 export default function Layout(props: PropsWithChildren) {
@@ -11,12 +11,10 @@ export default function Layout(props: PropsWithChildren) {
   const { currentPrivilege } = useUserClientProviderContext();
 
   const hasPrivilege = currentPrivilege?.some((privilege) =>
-    [ROLE_NETWORK_ADMIN, ROLE_COMPANY_ADMIN, ROLE_AGENT]?.includes(privilege)
+    [ROLE_NETWORK_ADMIN, ROLE_COMPANY_ADMIN]?.includes(privilege)
   );
 
-  if (!hasPrivilege && pathname.includes("/user")) {
-    return redirect("/");
-  }
+  if (!hasPrivilege && pathname === "/user") return redirect("/");
 
   return <>{props.children}</>;
 }
