42fa67d084dd16b98558b9d960b3c9c7bdf2f7e9
Wayan Danyael Eco <wayan.eco@98labs.com>
2024-08-29 22:15:25 +0800

    revert uselist table

--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -5,10 +5,31 @@ import { UserListTable } from "./components/user-list-table";
 
 import api from "utils/api";
 
+const getUsers = async () => {
+  const response = await api.get(`/api/user`, {
+    headers: headers(),
+    next: {
+      tags: ["user_list"],
+    },
+    cache: "no-store",
+  });
+
+  return response;
+};
 export default async function Page() {
+  const response = await getUsers();
+
+  if (!response.ok) {
+    throw new Error("Error fetching user list!");
+  }
+
+  const usersList = await response.json();
+
   return (
     <div className="pt-16 pb-12">
-      <Card></Card>
+      <Card>
+        <UserListTable data={usersList} />
+      </Card>
     </div>
   );
 }
