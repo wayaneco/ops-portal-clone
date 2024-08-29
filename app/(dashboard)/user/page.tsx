266d0d9c36ea2b76eaf78c4c530e62658bb33a39
Wayan Danyael Eco <wayan.eco@98labs.com>
2024-08-29 22:16:38 +0800

    revert uselist table

--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -2,10 +2,11 @@ import { Card } from "flowbite-react";
 import { headers } from "next/headers";
 
 import { UserListTable } from "./components/user-list-table";
+import { UserDetailType } from "@/app/types";
 
 import api from "utils/api";
 
-const getUsers = async () => {
+const getUsers = async (): Promise<Array<UserDetailType>> => {
   const response = await api.get(`/api/user`, {
     headers: headers(),
     next: {
@@ -14,21 +15,21 @@ const getUsers = async () => {
     cache: "no-store",
   });
 
-  return response;
-};
-export default async function Page() {
-  const response = await getUsers();
-
   if (!response.ok) {
     throw new Error("Error fetching user list!");
   }
 
-  const usersList = await response.json();
+  const data = await response.json();
+
+  return data;
+};
+export default async function Page() {
+  const users = await getUsers();
 
   return (
     <div className="pt-16 pb-12">
       <Card>
-        <UserListTable data={usersList} />
+        <UserListTable data={users} />
       </Card>
     </div>
   );
