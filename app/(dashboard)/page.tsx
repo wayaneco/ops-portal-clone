5e6437753881913b822ac0eec7065a91f2be22ae
Wayan Danyael Eco <wayan.eco@98labs.com>
2024-08-29 16:46:34 +0800

    Convert landing page to client

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,25 +1,30 @@
-import { createClient } from "@/utils/supabase/server";
+"use client";
+
 import { Button } from "flowbite-react";
 
-export default async function Page() {
-  const supabase = createClient();
+import { useSupabaseSessionContext } from "../components/Context/SupabaseSessionProvider";
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+const Page = () => {
+  const { user } = useSupabaseSessionContext();
 
   return (
     <div className="py-32">
-      <div className="text-5xl text-gray-500">Welcome, {user?.email}</div>
-      <div className="text-3xl text-gray-400 mt-10">
-        You&apos;ve been invited to receive credentials
-        <br /> for the Everest Network
-      </div>
-      <div className="mt-20">
-        <Button color="primary" size="lg">
-          Accept Passkey
-        </Button>
-      </div>
+      {user && (
+        <>
+          <div className="text-5xl text-gray-500">Welcome, {user?.email}</div>
+          <div className="text-3xl text-gray-400 mt-10">
+            You&apos;ve been invited to receive credentials
+            <br /> for the Everest Network
+          </div>
+          <div className="mt-20">
+            <Button color="primary" size="lg">
+              Accept Passkey
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
+
+export default Page;
