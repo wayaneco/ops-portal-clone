de632f0c92c554aa0304177425864d9eb2be9682
Wayan Danyael Eco <68528601+wayaneco@users.noreply.github.com>
2024-09-05 17:20:48 +0800

    Poc email resend (#6)
    * minor fix
    
    * prevent image from caching
    
    * fix transparent text on revoke modal
    
    * fix minor issue
    
    * initial commit
    
    * Send email functionality
    
    ---------
    
    Co-authored-by: xtiandanos <cris.danos@98labs.com>

--- app/(dashboard)/user/[id]/components/revoke-client.tsx
+++ app/(dashboard)/user/[id]/components/revoke-client.tsx
@@ -32,7 +32,7 @@ export const RevokeClient = () => {
               </div>
             </div>
           )}
-          <div className="text-center text-lg">
+          <div className="text-center text-lg text-gray-600">
             Do you want to revoke access to <strong>{client?.name}</strong> for{" "}
             <strong>{user?.email}</strong>?
           </div>
@@ -56,7 +56,7 @@ export const RevokeClient = () => {
               showToast({
                 message: (
                   <>
-                    <strong>{client?.name}</strong> has been revoke on{" "}
+                    <strong>{client?.name}</strong> has been revoked on{" "}
                     <strong>{user?.email}</strong>
                   </>
                 ),
---
+++ app/actions/email/invite.ts
@@ -0,0 +1,69 @@
+"use server";
+
+import { Resend } from "resend";
+
+import { EmailTemplate } from "@/components/EmailTemplate";
+import { createClient } from "@supabase/supabase-js";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const supabaseUrl = process.env["NEXT_PUBLIC_SUPABASE_URL"] as string;
+const supabaseRoleKey = process.env[
+  "NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY"
+] as string;
+const baseUrl = process.env["NEXT_PUBLIC_APP_BASE_URL"] as string;
+
+type InviteUserTypes = {
+  fullName: string;
+  client: string;
+  role: string;
+  email: string;
+  password: string;
+};
+
+export async function inviteUser({
+  fullName,
+  client,
+  role,
+  email,
+  password,
+}: InviteUserTypes) {
+  const supabaseAdmin = createClient(supabaseUrl, supabaseRoleKey);
+  try {
+    const { data, error: generate_link_error } =
+      await supabaseAdmin.auth.admin.generateLink({
+        type: "signup",
+        email,
+        password,
+      });
+
+    if (generate_link_error) throw generate_link_error?.message;
+
+    const { error } = await resend.emails.send({
+      from: "Everest Effect <onboarding@resend.dev>",
+      to: [email],
+      subject: "You have been invited",
+      react: EmailTemplate({
+        email,
+        fullName,
+        client,
+        role,
+        password,
+        confirmationLink: `${baseUrl}/login?token_hash=${data?.properties?.hashed_token}`,
+      }),
+    });
+
+    console.log("eeee", error);
+    if (error) throw error?.message;
+
+    return {
+      ok: true,
+      message: "Success",
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: error,
+    };
+  }
+}
