7e37c307c45fe6179f28c7e3d9628aae04311aa0
Wayan Danyael Eco <wayan.eco@98labs.com>
2024-10-18 20:46:46 +0800

    add logs

--- app/api/webhook/sms/route.ts
+++ app/api/webhook/sms/route.ts
@@ -4,8 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   const supabase = createClient();
 
-  console.log("BODY", request.body);
-  console.log("JSON", request.json());
+  console.log("request", await request);
+  console.log("BODY", await request.body);
+  console.log("JSON", await request.json());
 
   return NextResponse.json({
     status: 200,
